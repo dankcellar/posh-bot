99ef52080297701c9d0b9a4810e0efa5800cabac
Eric Downey <edowney29@gmail.com>
2020-05-07 14:49:30 -0400

    scroll to bottom and added posh filters

--- content.js
+++ content.js
@@ -1,50 +1,60 @@
 "use strict";
 
-let running = false;
+let checkerInterval = null;
 let shouldRun = false;
 let searchClass = null;
+let lastCount = 0;
+let goodChecks = 0;
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(
-    sender.tab
-      ? "from a content script:" + sender.tab.url
-      : "from the extension"
-  );
-  console.log(request)
-  getClassByType(request.type);
+  // console.log(
+  //   sender.tab
+  //     ? "from a content script:" + sender.tab.url
+  //     : "from the extension"
+  // );
+  console.log(request);
+
   shouldRun = request.toggle;
+  if (request.type === "party") {
+    searchClass = ".pm-party-share-link";
+  } else if (request.type === "follow") {
+    searchClass = ".pm-followers-share-link";
+  } else {
+    searchClass = null;
+  }
+
+  if (shouldRun) {
+    startInterval();
+  } else {
+    clearInterval(checkerInterval);
+  }
+
   sendResponse({ request });
 });
 
-function getClassByType(type) {
-  if (type === "party") searchClass = ".pm-party-share-link";
-  else if (type === "follow") searchClass = ".pm-followers-share-link";
-  else searchClass = null;
-}
+// function shuffle(arra1) {
+//   var ctr = arra1.length,
+//     temp,
+//     index;
+//   while (ctr > 0) {
+//     index = Math.floor(Math.random() * ctr);
+//     ctr--;
+//     temp = arra1[ctr];
+//     arra1[ctr] = arra1[index];
+//     arra1[index] = temp;
+//   }
+//   return arra1;
+// }
 
-function shuffle(arra1) {
-  var ctr = arra1.length,
-    temp,
-    index;
-  while (ctr > 0) {
-    index = Math.floor(Math.random() * ctr);
-    ctr--;
-    temp = arra1[ctr];
-    arra1[ctr] = arra1[index];
-    arra1[index] = temp;
-  }
-  return arra1;
-}
+// function getRandomInt(min, max) {
+//   // The maximum is exclusive and the minimum is inclusive
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-
-function startSharing() {
-  let counter = 0;
-  const elems = $(".share");
+function startSharing(elems) {
+  // const elems = $(".share");
   elems.each(function (index, element) {
     setTimeout(() => {
       if (shouldRun && searchClass) {
@@ -53,21 +63,44 @@ function startSharing() {
         element.focus();
         element.click();
         setTimeout(() => {
-          const share = $(searchClass).get(0);
-          console.log(share, searchClass);
-          share.focus();
-          share.click();
-        }, getRandomInt(1500, 2000));
+          //console.log(index, shouldRun, searchClass);
+          if (shouldRun && searchClass) {
+            const share = $(searchClass);
+            if (share) {
+              const htmlElem = share.get(0);
+              htmlElem.focus();
+              htmlElem.click();
+            }
+          }
+        }, 2500);
       }
-      counter++;
-      if (counter === elems.length) running = false;
-    }, getRandomInt(1500, 2000) * (index + 1));
+    }, 2500);
   });
 }
 
-setInterval(() => {
-  if (!running && shouldRun) {
-    running = true;
-    startSharing();
-  }
-}, 1000);
+function startInterval() {
+  lastCount = 0;
+  goodChecks = 0;
+
+  const radioToggle = $("#availability-available");
+  if (!radioToggle.prop("checked")) radioToggle.get(0).click();
+
+  checkerInterval = setInterval(() => {
+    const elems = $(".share");
+    if (lastCount === elems.length) {
+      goodChecks += 1;
+    } else {
+      scrollToElement(elems.last());
+    }
+    if (goodChecks === 5) {
+      clearInterval(checkerInterval);
+      startSharing(elems);
+    }
+    lastCount = elems.length;
+  }, 2500);
+}
+
+function scrollToElement(element) {
+  const htmlElem = element.get(0);
+  htmlElem.scrollIntoView();
+}
--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function sendMessage(message) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     console.log(tabs);
