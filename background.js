5f74ece44df02e44db9d16e772ac1e835ebe9817
Eric Downey <edowney29@gmail.com>
2020-05-07 16:18:44 -0400

    clean up

--- background.js
+++ background.js
@@ -1,17 +1,17 @@
 "use strict";
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
             pageUrl: { hostEquals: "poshmark.com", schemes: ["https"] },
-            css: ["a"]
-          })
+            css: ["a"],
+          }),
         ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      }
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
     ]);
   });
 });
