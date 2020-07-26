b6e590d8e6f85cb9d6df18a272cee1322eca2775
Eric Downey <edowney29@gmail.com>
2020-07-25 20:54:46 -0400

    Update content.js

--- content.js
+++ content.js
@@ -1,120 +1,120 @@
-"use strict";
+'use strict'
 
-let checkerInterval = null;
-let shouldRun = false;
-let searchClass = null;
-let lastCount = 0;
-let goodChecks = 0;
-let numLoops = 0;
-let shouldLoop = false;
+let checkerInterval = null
+let lastCount = 0
+let goodChecks = 0
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
-
-  shouldRun = request.toggle;
-  if (request.type === "party") {
-    searchClass = ".pm-party-share-link";
-    numLoops = 10;
-    shouldLoop = true;
-  } else if (request.type === "follow") {
-    searchClass = ".pm-followers-share-link";
-    numLoops = 1;
-    shouldLoop = false;
-  } else {
-    searchClass = null;
-    numLoops = 0;
-    shouldLoop = false;
-  }
-
-  if (shouldRun) {
-    startInterval();
-  } else {
-    clearInterval(checkerInterval);
-  }
-
-  sendResponse({ request });
-});
-
-function shuffle(array) {
-  var currentIndex = array.length;
-  var temporaryValue, randomIndex;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
+const DATA = {
+  follow: {
+    toggle: false,
+    search: '[data-et-name="share_poshmark"]',
+    loops: 1,
+  },
+  party: {
+    toggle: false,
+    search: '[data-et-name="share_poshmark_poshparty"]',
+    loops: 10,
+  },
 }
 
-// function getRandomInt(min, max) {
-//   // The maximum is exclusive and the minimum is inclusive
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min)) + min;
-// }
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  // console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension')
+  if (request.type === 'follow') {
+    DATA.follow.toggle = !DATA.follow.toggle
+    DATA.party.toggle = false
+  }
 
-function startSharing(_elems) {
-  // const elems = $(".share");
-  const elems = !shouldLoop ? shuffle(_elems) : _elems;
-  let total = 0;
-  let counter = 0;
-  elems.each(function (index, element) {
-    const countElem = $('<span class="count">0</span>');
-    countElem.insertAfter(element);
-    for (let i = 0; i < numLoops; i += 1) {
+  if (request.type === 'party') {
+    DATA.follow.toggle = false
+    DATA.party.toggle = !DATA.party.toggle
+  }
+
+  sendResponse({ follow: DATA.follow.toggle, party: DATA.party.toggle })
+  if (request.type) {
+    if (DATA.follow.toggle || DATA.party.toggle) startInterval()
+    else clearInterval(checkerInterval)
+  }
+})
+
+const shuffle = (array) => {
+  var currentIndex = array.length
+  var temporaryValue, randomIndex
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex--)
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+  return array
+}
+
+const createCounter = (elem) => {
+  const countElem = elem.closest('span.count')
+  if (countElem.length > 0) {
+    // countElem.text(`${i + 1}`)
+  } else {
+    const countElem = $('<span class="count">0</span>')
+    countElem.insertAfter(element)
+  }
+}
+
+const getLoops = () => {
+  if (DATA.follow.toggle) return DATA.follow.loops
+  if (DATA.party.toggle) return DATA.party.loops
+  return 0
+}
+
+const searchClass = () => {
+  if (DATA.follow.toggle) return $(DATA.follow.search)
+  if (DATA.party.toggle) return $(DATA.party.search)
+  return null
+}
+
+const startSharing = (_elems) => {
+  let total = 0
+  let counter = 0
+  const elems = DATA.party.toggle ? shuffle(_elems) : _elems
+  elems.each((index, element) => {
+    for (let i = 0; i < getLoops(); ++i) {
       setTimeout(() => {
-        if (shouldRun && searchClass) {
-          element.style.backgroundColor = "yellow";
-          element.style.color = "yellow";
-          element.focus();
-          element.click();
+        if (DATA.follow.toggle || DATA.party.toggle) {
+          scrollToElement(element)
+          element.click()
           setTimeout(() => {
-            //console.log(index, shouldRun, searchClass);
-            if (shouldRun && searchClass) {
-              const share = $(searchClass);
-              if (share) {
-                const htmlElem = share.get(0);
-                htmlElem.focus();
-                htmlElem.click();
-                countElem.text(`${i + 1}`);
-                counter += 1;
-                if (counter == total && shouldLoop) {
-                  startSharing(_elems);
-                }
+            const share = searchClass()
+            if (share) {
+              share.get(0).click()
+              if (total === counter++) {
+                startSharing(_elems)
               }
             }
-          }, 2500);
+          }, 2500)
         }
-      }, 2500 * ++total);
+      }, 2500 * ++total)
     }
-  });
+  })
 }
 
-function startInterval() {
-  lastCount = 0;
-  goodChecks = 0;
+const startInterval = () => {
+  lastCount = 0
+  goodChecks = 0
 
-  const radioToggle = $("#availability-available");
-  if (!radioToggle.prop("checked")) radioToggle.get(0).click();
+  const radioToggle = $('[data-et-name="availability"]')[1]
+  radioToggle.click()
 
   checkerInterval = setInterval(() => {
-    const elems = $(".share");
-    if (lastCount === elems.length) {
-      goodChecks += 1;
-    } else {
-      scrollToElement(elems.last());
-    }
+    const elems = $('[data-et-name="share"')
+    if (lastCount === elems.length) ++goodChecks
+    else scrollToElement(elems.last())
+
     if (goodChecks === 2) {
-      clearInterval(checkerInterval);
-      startSharing(elems);
+      clearInterval(checkerInterval)
+      startSharing(elems)
     }
-    lastCount = elems.length;
-  }, 2500);
+    lastCount = elems.length
+  }, 2500)
 }
 
-function scrollToElement(element) {
-  const htmlElem = element.get(0);
-  htmlElem.scrollIntoView();
+const scrollToElement = (elem) => {
+  elem.get(0).scrollIntoView()
 }
