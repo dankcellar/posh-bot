f4cf5039a9090a306fda0dd50f09f4ab4c55030b
Eric Downey <edowney29@gmail.com>
2020-07-25 20:54:40 -0400

    Reformat code and update breaking classes

--- public/popup.js
+++ public/popup.js
@@ -1,53 +1,45 @@
-"use strict";
+'use strict'
 
-function sendMessage(message) {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    console.log(tabs);
-    chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
-      console.log(response);
-    });
-  });
+const DATA = {
+  follow: {
+    true: 'Follow Mode Active!',
+    false: 'Toggle Follow Mode',
+    button: $('#followButton'),
+  },
+  party: {
+    true: 'Party Mode Active!',
+    false: 'Toggle Party Mode',
+    button: $('#partyButton'),
+  },
 }
 
-let partyOn = false;
-let followOn = false;
+const toggleActive = (type, toggle) => {
+  const elem = DATA[type].button
+  elem.text(DATA[type][toggle])
+  if (toggle) elem.addClass('pure-button-active')
+  else elem.removeClass('pure-button-active')
+}
 
-const followButton = $("#followButton");
-const partyButton = $("#partyButton");
-followButton.text("Toggle Follow Mode");
-partyButton.text("Toggle Party Mode");
+const sendMessage = (message) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, message, (response) => {
+      const { follow, party } = response
+      toggleActive('follow', follow)
+      toggleActive('party', party)
+    })
+  })
+}
 
-followButton.on("click", function (event) {
-  event.preventDefault();
-  followOn = !followOn;
-  sendMessage({ type: "follow", toggle: followOn });
-  if (followOn) {
-    followButton.text("Follow Mode On");
-    followButton.addClass("pure-button-active");
+DATA.follow.button.on('click', (event) => {
+  event.preventDefault()
+  sendMessage({ type: 'follow' })
+})
 
-    partyButton.text("Toggle Party Mode");
-    partyButton.removeClass("pure-button-active");
-  } else {
-    followButton.text("Toggle Follow Mode");
-    followButton.removeClass("pure-button-active");
-  }
-});
+DATA.party.button.on('click', (event) => {
+  event.preventDefault()
+  sendMessage({ type: 'party' })
+})
 
-partyButton.on("click", function (event) {
-  event.preventDefault();
-  partyOn = !partyOn;
-  sendMessage({ type: "party", toggle: partyOn });
-  if (partyOn) {
-    partyButton.text("Party Mode On");
-    partyButton.addClass("pure-button-active");
-
-    followButton.text("Toggle Follow Mode");
-    followButton.removeClass("pure-button-active");
-  } else {
-    partyButton.text("Toggle Party Mode");
-    partyButton.removeClass("pure-button-active");
-  }
-});
-
-const version = chrome.runtime.getManifest().version;
-const versionText = $("#versionText").text(`Version: ${version}`);
+const version = chrome.runtime.getManifest().version
+const versionText = $('#versionText').text(`Version: ${version}`)
+sendMessage({ type: null })
